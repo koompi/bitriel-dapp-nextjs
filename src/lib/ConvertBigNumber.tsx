300f4731b7b33c06028f4292bda3bf409cab672f
bendn <bend.n@outlook.com>
2024-01-24 21:28:29 +0700

    fix string manipulation in convertbignumber

--- src/lib/ConvertBigNumber.tsx
+++ src/lib/ConvertBigNumber.tsx
@@ -1,6 +1,6 @@
 export default function ConvertBigNumber(num: number): number {
-  const value = num / 1000000000000000000000;
-  const truncatedNumber = Number(value.toString().slice(0, -10));
+  const num = num / 1000000000000000000000;
+  const truncatedNumber = Math.floor(num / Math.pow(10, Math.floor(Math.log10(num)) + 1 - 10));
 
   return truncatedNumber;
 }
