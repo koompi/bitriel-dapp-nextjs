bb4e1ec4ab8a39476950fdcbf4e8caaa72815e94
bendn <bend.n@outlook.com>
2024-01-24 21:30:38 +0700

    cant shadow

--- src/lib/ConvertBigNumber.tsx
+++ src/lib/ConvertBigNumber.tsx
@@ -1,6 +1,6 @@
 export default function ConvertBigNumber(num: number): number {
-  const num = num / 1000000000000000000000;
-  const truncatedNumber = Math.floor(num / Math.pow(10, Math.floor(Math.log10(num)) + 1 - 10));
+  const n = num / 1000000000000000000000;
+  const truncatedNumber = Math.floor(n / Math.pow(10, Math.floor(Math.log10(n)) + 1 - 10));
 
   return truncatedNumber;
 }
